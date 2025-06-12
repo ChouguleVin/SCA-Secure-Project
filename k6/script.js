3ced1ec5c326d0ed25b2909e254ac273552cf5e2
ChouguleVin <vinayakac143@gmail.com>
2025-06-12 07:10:30 +0000

    one more

--- k6/script.js
+++ k6/script.js
@@ -7,7 +7,7 @@ export let options = {
 };
 
 export default function () {
-  let res = http.get('http://your-app-url');
+  let res = http.get('http://18.214.171.163:8080');
   check(res, { 'status was 200': (r) => r.status === 200 });
   sleep(1);
 }
